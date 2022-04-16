bfeaf796af07ef630a97c11ad4214226038912dc
Sanjid <sanjidk24@gmail.com>
2022-04-16 23:42:44 +0600

    Header part completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './shared/Header/Header';
+import Footer from './shared/Footer/Footer';
+import NotFound from './shared/NotFound/NotFound';
+import Home from './pages/Home/Home';
+import Services from './pages/Services/Services';
+import Blogs from './pages/Blogs/Blogs';
+import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to App Page</h1>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/services" element={<Services></Services>} ></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>} ></Route>
+        <Route path="/about" element={<About></About>} ></Route>
+        <Route path="/login" element={<Login></Login>} ></Route>
+        <Route path="/signup" element={<Signup></Signup>} ></Route>
+        <Route path="*" element={<NotFound></NotFound>} ></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-            <h1>This is Header</h1>
-
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Wildlife Photographer</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/services">Services</Nav.Link>
+            <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+          </Nav>
+          <Nav>
+          <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
