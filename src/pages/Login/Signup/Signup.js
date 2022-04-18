82c9ea402e2502b4a2ca44c4ea817b7f15f25b19
Sanjid <sanjidk24@gmail.com>
2022-04-18 21:24:18 +0600

    signup problem solve

--- src/pages/Login/Signup/Signup.js
+++ src/pages/Login/Signup/Signup.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { Button, Form, Spinner } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from '../../../firebase.init';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import SocialLogin from "../SocialLogin/SocialLogin";
@@ -12,6 +12,11 @@ const Signup = () => {
   const passwordRef = useRef('');
   const navigate = useNavigate();
   const [ createUserWithEmailAndPassword, user, loading2, error2 ] = useCreateUserWithEmailAndPassword(auth,{sendEmailVerification: true});
+
+  
+  const location = useLocation();
+
+  let from = location.state?.from?.pathname || "/";
   
 
   const navigateLogin= () =>{
@@ -28,7 +33,7 @@ const Signup = () => {
   }
 
   if(user){
-    navigate('/home');
+    navigate(from, { replace: true });
   }
 
   if(loading2){
@@ -40,12 +45,15 @@ const Signup = () => {
       <h1 className="fw-bold text-center text-info">Sign Up Form</h1>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Your Name</Form.Label>
           <Form.Control ref={nameRef} type="text" placeholder="Your Name" required/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
           <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Your Password</Form.Label>
           <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
         </Form.Group>
         <Button variant="primary" type="submit">
@@ -54,7 +62,6 @@ const Signup = () => {
       </Form>
       <h6 className="mt-3"> Already have an account..? <Link to="/login" onClick={navigateLogin} className="text-primary pe-auto text-decoration-none" >Login Here</Link> </h6>
 
-      <SocialLogin></SocialLogin>
 
     </div>
   );
--- src/pages/Login/SocialLogin/SocialLogin.js
+++ src/pages/Login/SocialLogin/SocialLogin.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Spinner } from "react-bootstrap";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
 import google from '../../../images/google.png'
 
@@ -10,8 +10,11 @@ const SocialLogin = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
   const navigate = useNavigate();
+  const location = useLocation();
 
   let errorElement;
+  let from = location.state?.from?.pathname || "/";
+
 
   if (error) {
     errorElement = (
@@ -24,7 +27,7 @@ const SocialLogin = () => {
 }
 
   if (user) {
-    navigate('/home');
+    navigate(from, { replace: true });
   }
 
   return (
